868ae103f07c0b955f86177133e5796cce707de6
clz <1814786186@qq.com>
2019-03-11 10:43:12 +0800

    spring cloud config

---
+++ config-server/src/main/java/com/example/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ * @date 2019-03-11 10:40:05
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
