9cedab956200abe72749159c9e19166069063150
clz <1814786186@qq.com>
2019-03-11 18:03:20 +0800

    Config Server高可用

---
+++ eureka-server/src/main/java/com/example/springcloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author admin
+ * @date 2019-03-11 15:35:25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
