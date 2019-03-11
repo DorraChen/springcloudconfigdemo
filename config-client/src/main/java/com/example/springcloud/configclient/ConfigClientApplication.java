50900f289d7f3a6cb582315b3c939a86903fe65e
clz <1814786186@qq.com>
2019-03-11 13:31:22 +0800

    config client

---
+++ config-client/src/main/java/com/example/springcloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,28 @@
+package com.example.springcloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author admin
+ * @date 2019-03-11 10:40:05
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+	@RequestMapping(value = "/foo")
+	public String hi(){
+		return foo;
+	}
+
+}
--- config-server/src/main/java/com/example/springcloud/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/springcloud/configserver/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.example.springcloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author admin
  * @date 2019-03-11 10:40:05
  */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
